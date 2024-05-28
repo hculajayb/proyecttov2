690d212b12e46678efe6fe37baf96d4432025d47
hculajay <hculajayb@miumg.edu.gt>
2024-05-27 22:42:14 -0600

    Listo para nube, con pago simulado y titulo con fines educativos, BD en MongoDB Cloud

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -4,11 +4,12 @@ export const dbConnect = () => {
     //local
     //connect(process.env.MONGO_URI = 'mongodb://localhost:27017/foodmine', {
     //nube
-    connect(process.env.MONGO_URI = 'mongodb://mongo:sIFfvNvWAdtGmQZwkhBfarMcoJwALYJh@viaduct.proxy.rlwy.net:36740', {
+    //connect(process.env.MONGO_URI = 'mongodb://mongo:sIFfvNvWAdtGmQZwkhBfarMcoJwALYJh@viaduct.proxy.rlwy.net:36740', {
+    connect(process.env.MONGO_URI = 'mongodb+srv://foodie:EstoEsUnaPrueba2024@foods.2yyxz7y.mongodb.net/?retryWrites=true&w=majority&appName=foods', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     } as ConnectOptions).then(
-        () => console.log("connect successfully"),
+        () => console.log("Conectado a la BD con éxito!"),
         (error) => console.log(error)
     )
 }
